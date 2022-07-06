a6ec1ab381a7c439cc542041294b82b7900df489
Marcelo Soares <soaresmarcelo94@gmail.com>
2022-07-05 22:05:12 -0300

    add: root colors and font-family

--- src/styles/global.js
+++ src/styles/global.js
@@ -3,12 +3,17 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
         
     :root{
-       
-    }
+       --orange: #F2AE2F;
+       --green: #83B200;
+       --red: #F22222;
+       --petroleumBlue: #4DB1B3;
+       --white: #F2F2F2;
+    }  
     
     body {
         margin: 0;
         padding: 0;
+        font-family: 'Comfortaa';
     }
 
     button {
