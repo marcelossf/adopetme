6b5256b8135e2b7ca01401a03fa57ea3ebcee22d
Igor Garcia <igorgg@gmail.com>
2022-07-07 16:47:13 -0300

    fix: background and color schemas

--- src/components/Button/style.js
+++ src/components/Button/style.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const StyledButton = styled.button`
   padding: 0.5rem 2rem;
-  color: white;
+  color: ${props => props.blackSchema ? "#000" : "#ffff"};
   font-family: "Comfortaa";
   font-style: normal;
   font-weight: 700;
@@ -14,7 +14,8 @@ export const StyledButton = styled.button`
   box-shadow: 0px 4px 2px rgba(0, 0, 0, 0.25);
   border-radius: 10px;
   border: none;
-  background-color: var(--petroleumBlue);
+  background-color: ${(props)=> props.orangeSchema ? "#F2AE2F" : "#4DB1B3"};
+  width: ${props => props.width};
 
   &:hover {
     box-shadow: inset 0px 0px 19px 6px rgba(0, 0, 0, 0.63);
