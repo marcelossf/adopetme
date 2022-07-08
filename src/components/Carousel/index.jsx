119d36b8e0a2694e585aba636f6f51a5f84e99ef
Mari Spinola <mapincosf@gmail.com>
2022-07-07 22:04:21 -0300

    fix: cat image text

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,6 +1,6 @@
-// import Paw from "../../assets/pawsLogo.svg";
+import Paw from "../../assets/pawsLogo.svg";
 
-import { Autoplay, Navigation, Pagination } from "swiper";
+import { Autoplay } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -13,31 +13,30 @@ export const Carousel = () => {
       <SectionContainer>
         <CarouselContainer>
           <Swiper
-            navigation={true}
             autoplay={{ delay: 3000, disableOnInteraction: false }}
             spaceBetween={50}
-            modules={[Autoplay, Pagination, Navigation]}
+            modules={[Autoplay]}
             className="swiperContainer"
           >
             <SwiperSlide className="imgContainer">
               <div className="image cat">
-                <h1>Gato</h1>
+                <h2>Azar mesmo é não ter esse carinho!</h2>
               </div>
             </SwiperSlide>
             <SwiperSlide className="imgContainer" >
               <div className="image dog">
-                <h1>Cachorro</h1>
+                <h2>Cachorro</h2>
               </div>
             </SwiperSlide>
             <SwiperSlide className="imgContainer">
               <div className="image hamster">
-                <h1>hamster</h1>
+                <h2>hamster</h2>
               </div>
             </SwiperSlide>
           </Swiper>
         </CarouselContainer>
+      <img className="paws" src={Paw} alt="icone de pata" />
       </SectionContainer>
-      {/* <img className="paws" src={Paw} alt="" /> */}
     </>
   );
 };
--- src/components/Carousel/style.js
+++ src/components/Carousel/style.js
@@ -4,40 +4,47 @@ import dogSM from "../../assets/dog370.png";
 import dogLG from "../../assets/dogDesk.png";
 import dogMD from "../../assets/dogMD.png";
 
-import hamLG from "../../assets/hamLG.png";
-import hamMD from "../../assets/hamMD.png";
-import hamSM from "../../assets/hamSM.png";
-
 import catLG from "../../assets/catDesk.png";
 import catMD from "../../assets/catMD.png";
 import catSM from "../../assets/catSM.png";
 
+import hamLG from "../../assets/hamLG.png";
+import hamMD from "../../assets/hamMD.png";
+import hamSM from "../../assets/hamSM.png";
+
 
 export const SectionContainer = styled.section`
   width: 100%;
+  @media (min-width: 768px){
+    padding: 2em;
+  }
+  
+  @media (min-width: 425px) {
+    height: fit-content;
+    padding: 1em;
 
-  /* 
-  .paws {
-    position: relative;
-    top: -60px;
-    right: -250px;
-    z-index: 9999;
-    width: 141px;
-    height: 105px;
-
-    @media (min-width: 768px) {
-      right: -600px;
-    }
-  } */
-  @media (min-width: 1024px) {
-    height: 400px;
     display: flex;
     justify-content: center;
     align-items: center;
+    
     background-color: var(--petroleumBlue);
-    box-shadow: 0px 0px 40px px var(--petroleumBlue);
-    -webkit-box-shadow: 0px 0px 40px 22px var(--petroleumBlue);
-    -moz-box-shadow: 0px 0px 40px 22px var(--petroleumBlue);
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  }
+  
+  .paws {
+    position: absolute;
+    top: 49%;
+    right: 0;
+
+    z-index: 1;
+    width: 180px;
+    height: auto;
+
+    @media(min-width:425px) {  
+      top: 75%;
+    }
+
+
   }
 `;
 
@@ -47,47 +54,73 @@ export const CarouselContainer = styled.div`
 
   .swiperContainer {
     width: 100%;
-
   }
-
-    
-    /* border-top: 4px solid var(--orange);
-    border-bottom: 4px solid var(--orange);
-     */
 
   .imgContainer {
     width: 100%;
-    height: 250px;  
-    
-    /* position: relative; */
-    /* overflow: hidden; */
-    @media(min-width:1024px) {
-      margin: 0 auto;
-      width: 80%;
+    height: 300px;  
+    margin: 0 auto;
+
+    @media(min-width:425px) {  
+      border: 4px solid var(--orange);
+      border-radius: 30px;
+      width: 95%;
+    }
+    @media(max-width:425px){
+      border-top: 4px solid var(--orange);
+      border-bottom: 4px solid var(--orange);
     }
   }
+
   .image{
     height: 100%;
-    max-width: 100%;
-    
+    width: 100%;
+    margin: 0 auto;
+    opacity: 0.8;
+
     background: no-repeat center; 
     background-size: cover;
     
-    @media(min-width:1024px) {
-      margin: 0 auto;
+    @media(min-width:425px) {  
       border-radius: 30px;
-      width: 80%;
+    }
+    
+    h2{
+      color: var(--white);
+      width: 300px;
+      
+      font-size: 48px;
+      line-height: 58px;
+      letter-spacing: 0.06em;
+
+      text-shadow: 2px 2px 4px #000000;
+
+      @media(max-width: 768px){
+        font-size: 36px;
+
+      }
     }
 
   }
   
   .cat{
+    color: var(--white);
+    display: flex;
+    align-items: center;
+    justify-content: end;
+
     background-image:  url(${catLG});
+    
     @media(max-width:1024px){
       background-image: url(${catMD});
     }
     @media(max-width:425px){
       background-image: ${catSM};
+
+    }
+    h2{
+      text-align: right;
+      margin-right: 1em;
     }
   }
 
@@ -99,13 +132,12 @@ export const CarouselContainer = styled.div`
     }
 
     @media(max-width:425px){
+      background-position: right;
       background-image: ${dogSM};
     }
   }
     .hamster{
       background-image:  url(${hamLG});
-
-
 
       @media(max-width:1024px){
         background-image: url(${hamMD});
@@ -113,6 +145,7 @@ export const CarouselContainer = styled.div`
       
       @media(max-width:425px){
         background-image: ${hamSM};
+        background-position: right;
       }
     }
 `
