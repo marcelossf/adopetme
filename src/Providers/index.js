a2dc30c8445f381fbba4650ddff70661e972d068
Marcelo Soares <soaresmarcelo94@gmail.com>
2022-07-09 17:43:27 -0300

    merge: develop with feat-component-searchBar

--- src/Providers/index.js
+++ src/Providers/index.js
@@ -2,14 +2,17 @@ import React from "react";
 import { AnimalsListProvider } from "../context/animals";
 import { LoginLogoutProvider } from "../context/login-logout";
 import { RedirectProvider } from "../context/redirect";
+import { SearchProvider } from "./context/search";
 
 function Providers({ children }) {
   return (
     <>
       <AnimalsListProvider>
-        <RedirectProvider>
-          <LoginLogoutProvider>{children}</LoginLogoutProvider>
-        </RedirectProvider>
+        <SearchProvider>
+          <RedirectProvider>
+            <LoginLogoutProvider>{children}</LoginLogoutProvider>
+          </RedirectProvider>
+        </SearchProvider>
       </AnimalsListProvider>
     </>
   );
