cef1c3f9bac70f0f40844ffd3f7e50eba9ce6d0a
Igor Garcia <igorgg@gmail.com>
2022-07-09 19:21:16 -0300

    feat-condition render login and withou login

--- src/components/MenuMobile/index.jsx
+++ src/components/MenuMobile/index.jsx
@@ -1,11 +1,12 @@
+import { useContext } from "react";
 import LogoBlack from "../../assets/adoPetPreto.png";
 import Close from "../../assets/fechar.png";
+import { LoginLogoutContext } from "../../context/login-logout";
 import { LinksMenuMobile } from "../LinksMenuMobile";
 import { Section } from "./style";
 
 const MenuMobile = ({ setOpen }) => {
-
-  
+  const { logado } = useContext(LoginLogoutContext);
 
   const closeModal = () => {
     setOpen(false);
@@ -23,10 +24,21 @@ const MenuMobile = ({ setOpen }) => {
         ></img>
       </div>
       <div className="menu-links">
-        <p className="text-menu">Início</p>
-        <p className="text-menu">Login</p>
-        <p className="text-menu">Cadastrar</p>
-        <p className="text-menu">Quem somos</p>
+        {logado ? (
+          <>
+          <p className="text-menu">Início</p>
+          <p className="text-menu">Quero Adotar</p>
+          <p className="text-menu">Solicitações</p>
+          <p className="text-menu">Logout</p>
+        </>
+        ) : (
+          <>
+            <p className="text-menu">Início</p>
+            <p className="text-menu">Login</p>
+            <p className="text-menu">Cadastrar</p>
+            <p className="text-menu">Quem somos</p>
+          </>
+        )}
       </div>
     </Section>
   );
