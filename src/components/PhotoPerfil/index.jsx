3106a7139006b8a8ca4f314ab487dc1d360a00e9
Igor Garcia <igorgg@gmail.com>
2022-07-13 19:14:41 -0300

    fix: modal logout com nome

--- src/components/PhotoPerfil/index.jsx
+++ src/components/PhotoPerfil/index.jsx
@@ -1,13 +1,11 @@
-import { Modal } from "@mui/material";
 import { useState, useContext } from "react";
 import Profile from "../../assets/ProfileIcon.png";
 import { DivImg, ModalPerfil } from "./style";
-import { LoginLogoutContext } from "../../context/login-logout";
 import { UserContext } from "../../context/user";
 
 function PhotoPerfil() {
   const [open, setOpen] = useState(false);
-  const { logout } = useContext(UserContext);
+  const { logout, user } = useContext(UserContext);
 
   return (
     <>
@@ -20,7 +18,7 @@ function PhotoPerfil() {
         />
         {open ? (
           <ModalPerfil>
-            {"Name"}
+            {user.name}
             <button
               type="button"
               onClick={() => {
