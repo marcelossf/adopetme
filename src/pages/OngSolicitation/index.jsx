fb595b104ce44df9519dded918b87ca253abedf5
Mari Spinola <mapincosf@gmail.com>
2022-07-14 09:37:54 -0300

    empty cart

--- src/pages/OngSolicitation/index.jsx
+++ src/pages/OngSolicitation/index.jsx
@@ -1,98 +1,99 @@
-import CardRetangle from "../../components/CardRetangle";
-import { Footer } from "../../components/Footer";
-import OngTile from "../../components/OngTitle";
-import { Container } from "./style";
 import { useContext, useEffect, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { Button } from "../../components/Button";
+import CardRetangle from "../../components/CardRetangle";
+import { Footer } from "../../components/Footer";
 import HeaderDekstop from "../../components/HeaderDesktop";
 import HeaderMobile from "../../components/HeaderMobile";
 import { LinksMenu } from "../../components/MenuMobile/style.js";
+import OngTile from "../../components/OngTitle";
 import PhotoPerfil from "../../components/PhotoPerfil";
+import { SolicitationContext } from "../../context/solicitation";
 import { UserContext } from "../../context/user";
 import { MarginNavBar } from "../../styles/marginNavBar";
-import { SolicitationContext } from "../../context/solicitation";
+import { Container } from "./style";
 
 const OngSolicitation = () => {
-  const token = JSON.parse(localStorage.getItem("token"));
-  const { logout } = useContext(UserContext);
-  const [selected] = useState(true);
-  const history = useHistory();
-  const { solicitationData } = useContext(SolicitationContext);
-  const { user } = useContext(UserContext);
-  const ongName = user?.name;
+	const token = JSON.parse(localStorage.getItem("token"));
+	const { logout } = useContext(UserContext);
+	const [selected] = useState(true);
+	const history = useHistory();
+	const { solicitationData } = useContext(SolicitationContext);
+	const { user } = useContext(UserContext);
+	const ongName = user?.name;
 
-  const ongSolicitation = solicitationData.filter(
-    (solicitation) => solicitation?.petData.ong === ongName
-  );
+	const ongSolicitation = solicitationData.filter(
+		(solicitation) => solicitation?.petData.ong === ongName
+	);
 
-  const handleRoute = (route) => {
-    route && history.push(`/${route}`);
-  };
+	const handleRoute = (route) => {
+		route && history.push(`/${route}`);
+	};
 
-  useEffect(() => {
-    if (!token) {
-      history.push("/");
-    }
-  }, []);
+	useEffect(() => {
+		if (!token) {
+			history.push("/");
+		}
+	}, []);
 
-  return (
-    <Container>
-      <HeaderDekstop>
-        <Button
-          className="button-selected"
-          width={"230px"}
-          onClick={() => history.push("/ong")}
-        >
-          Início
-        </Button>
+	return (
+		<Container>
+			<HeaderDekstop>
+				<Button
+					className="button-selected"
+					width={"230px"}
+					onClick={() => history.push("/ong")}>
+					Início
+				</Button>
 
-        <Button
-          className="button-selected"
-          width={"230px"}
-          onClick={() => handleRoute("ong-solicitation")}
-        >
-          Solicitações
-        </Button>
-        <Button width={"230px"} onClick={() => handleRoute("registerPet")}>
-          Cadastrar Pet
-        </Button>
-        <PhotoPerfil />
-      </HeaderDekstop>
-      <HeaderMobile selected={selected}>
-        <LinksMenu selected={selected} onClick={() => history.push("/")}>
-          Início
-        </LinksMenu>
+				<Button
+					className="button-selected"
+					width={"230px"}
+					onClick={() => handleRoute("ong-solicitation")}>
+					Solicitações
+				</Button>
+				<Button
+					width={"230px"}
+					onClick={() => handleRoute("registerPet")}>
+					Cadastrar Pet
+				</Button>
+				<PhotoPerfil />
+			</HeaderDekstop>
+			<HeaderMobile selected={selected}>
+				<LinksMenu
+					selected={selected}
+					onClick={() => history.push("/")}>
+					Início
+				</LinksMenu>
 
-        <LinksMenu
-          className="link--selected "
-          onClick={() => handleRoute("ong-solicitation")}
-        >
-          Solicitações
-        </LinksMenu>
-        <LinksMenu onClick={() => handleRoute("registerPet")}>
-          Cadastrar Pet
-        </LinksMenu>
-        <LinksMenu onClick={() => logout()}>Logout</LinksMenu>
-      </HeaderMobile>
-      <MarginNavBar></MarginNavBar>
+				<LinksMenu
+					className="link--selected "
+					onClick={() => handleRoute("ong-solicitation")}>
+					Solicitações
+				</LinksMenu>
+				<LinksMenu onClick={() => handleRoute("registerPet")}>
+					Cadastrar Pet
+				</LinksMenu>
+				<LinksMenu onClick={() => logout()}>Logout</LinksMenu>
+			</HeaderMobile>
+			<MarginNavBar></MarginNavBar>
 
-      <OngTile />
-      {solicitationData.length < 0 ? (
-        <CardRetangle />
-      ) : (
-        <div>
-          <p>Você não tem nenhuma solicitação</p>
-        </div>
-      )}
+			<OngTile />
+			{solicitationData.length < 0 ? (
+				<CardRetangle />
+			) : (
+				<div className="empty">
+					<h2>Você não tem nenhuma solicitação :c</h2>
+				</div>
+			)}
 
-      <Footer>
-        <Link to="/">Início</Link>
-        <Link to="/ong-solicitation">Solicitações</Link>
-        <Link to="/registerPet">Cadastrar Pet</Link>
-      </Footer>
-    </Container>
-  );
+			<Footer>
+				<Link to="/">Início</Link>
+				<Link to="/ong-solicitation">Solicitações</Link>
+				<Link to="/registerPet">Cadastrar Pet</Link>
+			</Footer>
+		</Container>
+	);
 };
 
 export default OngSolicitation;
--- src/pages/OngSolicitation/style.js
+++ src/pages/OngSolicitation/style.js
@@ -1,8 +1,26 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  gap: 2rem;
+	.empty {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		background: rgba(77, 177, 179, 1);
+		border: 5px solid var(--orange);
+		color: white;
+
+		letter-spacing: 0.06em;
+
+		max-width: 1300px;
+		width: 100%;
+		border-radius: 15px;
+		margin: 0 auto;
+		height: 200px;
+	}
+
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	gap: 2rem;
 `;
